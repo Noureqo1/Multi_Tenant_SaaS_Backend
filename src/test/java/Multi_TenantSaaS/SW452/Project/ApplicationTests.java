cfc162baca630cf7963a1164e0a3dbcf98068c68
noureqo <132711539+Noureqo1@users.noreply.github.com>
2026-03-18 07:01:17 +0200

    Spring Setup.

---
+++ src/test/java/Multi_TenantSaaS/SW452/Project/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Multi_TenantSaaS.SW452.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
